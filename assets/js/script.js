8f24ab80d0509476672c1068ffa688ce0a01c509
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-03-30 12:58:45 +0200

    Build: Test data pulling via console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // fetch request to get data from starwarsapi
 
-fetch("https://swapi.dev/api")
-.then(response => response.json())
-.then(data => console.log(data))
+fetch("https://swapi.dev/api/planets/100")
+  .then((response) => {
+    if (response.ok) {
+      console.log("Successfully pulled data.");
+    } else {
+      console.log("Failed to pull data.");
+    }
+  })
+  .then((data) => console.log(data));
