cfb59a6488813d5648b163bff97fb28a175e3b88
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-03-30 13:28:55 +0200

    Build: Create two fetch requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // fetch request to get data from starwarsapi
 
-fetch("https://swapi.dev/api/planets")
+/**
+ * returns array of vehicle data
+ */
+
+fetch("https://swapi.dev/api/vehicles")
   .then((response) => {
     if (response.ok) {
       return response.json();
@@ -8,4 +12,29 @@ fetch("https://swapi.dev/api/planets")
       console.log("Failed to pull data.");
     }
   })
-  .then((data) => console.log(data));
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+/**
+ * returns array of starship data
+ */
+
+fetch("https://swapi.dev/api/starships")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log("Failed to pull data.");
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
