4717384003871d06a16351cab9481257def8f298
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-03-30 13:03:28 +0200

    Build: Correctly return data + call errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // fetch request to get data from starwarsapi
 
-fetch("https://swapi.dev/api/planets/100")
+fetch("https://swapi.dev/api/planets")
   .then((response) => {
     if (response.ok) {
-      console.log("Successfully pulled data.");
+      return response.json();
     } else {
       console.log("Failed to pull data.");
     }
