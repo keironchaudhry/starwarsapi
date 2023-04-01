a5164d8dfe196f4a761943a94de265e2956e895f
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-04-01 15:25:17 +0200

    Feat: Render API data for starships

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,29 @@ fetch("https://swapi.dev/api/vehicles/")
   .catch((error) => {
     console.error(error);
   });
+
+/**
+ * returns array of starship data
+ */
+
+fetch("https://swapi.dev/api/starships/")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log("Failed to pull data.");
+    }
+  })
+  .then((data) => {
+    const starships = data.results;
+    console.log(starships);
+    const starshipList = document.getElementById("starship-list");
+    for (const starship of starships) {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `<strong>${starship.name}</strong> (Model: ${starship.model}, Crew: ${starship.crew}, Passengers: ${starship.passengers})`;
+      starshipList.appendChild(listItem);
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
